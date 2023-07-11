c4930c5c2d881258e7448befe2f7d334b1746420
PEXAVC <89366826+pexavc@users.noreply.github.com>
2023-07-11 11:12:21 -0700

    remove GraniteML

--- Sources/GraniteML/Architecture/Executable.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public protocol Executable {
-    var label : String { get }
-    
-    func run(on input : Any, state : GranitePipelineState) throws -> Any?
-    
-    func runBatch(on inputs: [Any], state : GranitePipelineState) throws -> [Any]?
-}
